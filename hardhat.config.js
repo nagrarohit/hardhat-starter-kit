a9869774912677a07ecf3e7408cf7bd8b0052911
nagrarohit <rohit.nagra7861@gmail.com>
2023-06-02 21:22:23 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,40 +1,58 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
+require("@nomicfoundation/hardhat-toolbox")
 require("./tasks")
-require("@appliedblockchain/chainlink-plugins-fund-link")
 require("dotenv").config()
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const COMPILER_SETTINGS = {
+    optimizer: {
+        enabled: true,
+        runs: 1000000,
+    },
+    metadata: {
+        bytecodeHash: "none",
+    },
+}
 
 const MAINNET_RPC_URL =
     process.env.MAINNET_RPC_URL ||
     process.env.ALCHEMY_MAINNET_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
 const POLYGON_MAINNET_RPC_URL =
     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL;
+const MUMBAI_RPC_URL =
+    process.env.MUMBAI_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 // optional
 const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
-const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
+const FORKING_BLOCK_NUMBER = parseInt(process.env.FORKING_BLOCK_NUMBER) || 0
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
 const REPORT_GAS = process.env.REPORT_GAS || false
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    defaultNetwork: "hardhat",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+                COMPILER_SETTINGS,
+            },
+            {
+                version: "0.6.6",
+                COMPILER_SETTINGS,
+            },
+            {
+                version: "0.4.24",
+                COMPILER_SETTINGS,
+            },
+        ],
+    },
     networks: {
         hardhat: {
+            hardfork: "merge",
             // If you want to do some forking set `enabled` to true
             forking: {
                 url: MAINNET_RPC_URL,
@@ -46,23 +64,13 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //accounts: {
-            //     mnemonic: MNEMONIC,
-            // },
-            saveDeployments: true,
-            chainId: 42,
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
+        sepolia: {
+            url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             //   accounts: {
             //     mnemonic: MNEMONIC,
             //   },
-            saveDeployments: true,
-            chainId: 4,
+            chainId: 11155111,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
@@ -70,22 +78,28 @@ module.exports = {
             //   accounts: {
             //     mnemonic: MNEMONIC,
             //   },
-            saveDeployments: true,
             chainId: 1,
         },
         polygon: {
             url: POLYGON_MAINNET_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
             chainId: 137,
         },
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            chainId: 80001,
+        },
     },
+    defaultNetwork: "hardhat",
     etherscan: {
         // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
         apiKey: {
-            rinkeby: ETHERSCAN_API_KEY,
-            kovan: ETHERSCAN_API_KEY,
+            // npx hardhat verify --list-networks
+            sepolia: ETHERSCAN_API_KEY,
+            mainnet: ETHERSCAN_API_KEY,
             polygon: POLYGONSCAN_API_KEY,
+            polygonMumbai: POLYGONSCAN_API_KEY,
         },
     },
     gasReporter: {
@@ -97,31 +111,22 @@ module.exports = {
     },
     contractSizer: {
         runOnCompile: false,
-        only: ["APIConsumer", "KeepersCounter", "PriceConsumerV3", "RandomNumberConsumer"],
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
-        feeCollector: {
-            default: 1,
-        },
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.7",
-            },
-            {
-                version: "0.6.6",
-            },
-            {
-                version: "0.4.24",
-            },
+        only: [
+            "APIConsumer",
+            "AutomationCounter",
+            "NFTFloorPriceConsumerV3",
+            "PriceConsumerV3",
+            "RandomNumberConsumerV2",
+            "RandomNumberDirectFundingConsumerV2",
         ],
     },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./build/cache",
+        artifacts: "./build/artifacts",
+    },
     mocha: {
-        timeout: 200000, // 200 seconds max for running tests
+        timeout: 300000, // 300 seconds max for running tests
     },
 }
